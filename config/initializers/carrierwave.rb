360940a07a13248f7c69961bcd61e156e1306b9e
tom10sky <super_express527@yahoo.co.jp>
2019-11-13 19:10:44 +0900

    setting s3-3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chatspace527'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace527'
+  config.fog_directory  = 'chatspaceimage527'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspaceimage527'
 end
